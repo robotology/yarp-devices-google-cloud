9b16e70fe5bec5b6771f7dbcb876fc26b70bedf8
Ettore Landini <ettore.landini@iit.it>
2023-07-28 16:06:52 +0200

    First tests
    The audio gets generated correctly

--- src/devices/googleSpeechSynthesizer/GoogleSpeechSynthesizer.cpp
+++ src/devices/googleSpeechSynthesizer/GoogleSpeechSynthesizer.cpp
@@ -9,8 +9,11 @@
 
 #include "GoogleSpeechSynthesizer.h"
 
+#include <yarp/sig/SoundFile.h>
+
 #include <yarp/os/LogComponent.h>
 #include <yarp/os/LogStream.h>
+#include <fstream>
 
 #include <cmath>
 
@@ -29,12 +32,114 @@ GoogleSpeechSynthesizer::GoogleSpeechSynthesizer()
 
 bool GoogleSpeechSynthesizer::open(yarp::os::Searchable &config)
 {
-    yCError(GOOGLESPEECHSYNTH) << "Open";
+    m_synthClient = std::make_shared<texttospeech::TextToSpeechClient>(texttospeech::MakeTextToSpeechConnection());
+    m_synthVoiceSelParams = std::make_shared<google::cloud::texttospeech::v1::VoiceSelectionParams>();
+    m_synthVoice = std::make_shared<google::cloud::texttospeech::v1::Voice>();
+    m_synthInput = std::make_shared<google::cloud::texttospeech::v1::SynthesisInput>();
+    m_synthAudioConfig = std::make_shared<google::cloud::texttospeech::v1::AudioConfig>();
+
+    m_synthVoiceSelParams->set_language_code("en-US");
+    m_synthVoiceSelParams->set_name("en-US-Wavenet-C");
+
+    m_synthAudioConfig->set_audio_encoding(google::cloud::texttospeech::v1::MP3);
+
     return true;
 }
 
 bool GoogleSpeechSynthesizer::close()
 {
-    yCError(GOOGLESPEECHSYNTH) << "Close";
+    return true;
+}
+
+bool GoogleSpeechSynthesizer::setLanguage(const std::string& language)
+{
+    m_synthVoiceSelParams->set_language_code(language);
+
+    return true;
+}
+
+bool GoogleSpeechSynthesizer::getLanguage(std::string& language)
+{
+    language = m_synthVoiceSelParams->language_code();
+
+    return true;
+}
+
+bool GoogleSpeechSynthesizer::setVoice(const std::string& voice_name)
+{
+    m_synthVoiceSelParams->set_name(voice_name);
+
+    return true;
+}
+
+bool GoogleSpeechSynthesizer::getVoice(std::string& voice_name)
+{
+    voice_name = m_synthVoiceSelParams->name();
+
+    return true;
+}
+
+bool GoogleSpeechSynthesizer::setSpeed(const double speed)
+{
+    m_synthAudioConfig->set_speaking_rate(speed);
+    if(m_synthAudioConfig->speaking_rate() != speed)
+    {
+        yCError(GOOGLESPEECHSYNTH) << "Error while setting the speach rate (speed)";
+        return false;
+    }
+
+    return true;
+}
+
+bool GoogleSpeechSynthesizer::getSpeed(double& speed)
+{
+    speed = m_synthAudioConfig->speaking_rate();
+
+    return true;
+}
+
+bool GoogleSpeechSynthesizer::setPitch(const double pitch)
+{
+    m_synthAudioConfig->set_pitch(pitch);
+    if(m_synthAudioConfig->pitch() != pitch)
+    {
+        yCError(GOOGLESPEECHSYNTH) << "Error while setting the pitch value";
+        return false;
+    }
+
+    return true;
+}
+
+bool GoogleSpeechSynthesizer::getPitch(double& pitch)
+{
+    pitch = m_synthAudioConfig->pitch();
+
+    return true;
+}
+
+bool GoogleSpeechSynthesizer::synthesize(const std::string& text, yarp::sig::Sound& sound)
+{
+    YARP_UNUSED(sound);
+    m_synthInput->set_text(text);
+    yCDebug(GOOGLESPEECHSYNTH) << m_synthVoiceSelParams->language_code();
+    google::cloud::v2_14::StatusOr<google::cloud::texttospeech::v1::SynthesizeSpeechResponse> response = m_synthClient->SynthesizeSpeech(*m_synthInput,*m_synthVoiceSelParams,*m_synthAudioConfig);
+    if (!response) {
+        yCError(GOOGLESPEECHSYNTH) << "Error synthesizing speech. Google status:\n\t" << response.status().message() << "\n";
+        return false;
+    }
+
+    /// TODO: REMOVE
+    std::string filename = "hello_world.mp3";
+    std::ofstream outfile(filename, std::ios::binary);
+    outfile.write(response->audio_content().data(), response->audio_content().size());
+    outfile.close();
+    /// TODO: REMOVE
+
+    sound.clear();
+    if(!yarp::sig::file::read_bytestream(sound, response->audio_content().data(), response->audio_content().size(), ".mp3"))
+    {
+        yCError(GOOGLESPEECHSYNTH) << "Error while transfering data from google response to yarp::sigSound sound object";
+    }
+
     return true;
 }
--- src/devices/googleSpeechSynthesizer/GoogleSpeechSynthesizer.h
+++ src/devices/googleSpeechSynthesizer/GoogleSpeechSynthesizer.h
@@ -7,10 +7,15 @@
 #define YARP_GOOGLESPEECHSYNTH_H
 
 #include <yarp/dev/DeviceDriver.h>
+#include <yarp/dev/ISpeechSynthesizer.h>
+#include <yarp/sig/Sound.h>
+#include <yarp/os/Network.h>
+
 #include "google/cloud/texttospeech/v1/text_to_speech_client.h"
 
 class GoogleSpeechSynthesizer :
-        public yarp::dev::DeviceDriver
+        public yarp::dev::DeviceDriver,
+        public yarp::dev::ISpeechSynthesizer
 {
 public:
     GoogleSpeechSynthesizer();
@@ -23,6 +28,29 @@ public:
     // DeviceDriver
     bool open(yarp::os::Searchable& config) override;
     bool close() override;
+
+    // yarp::dev::ISpeechSynthesizer
+    bool setLanguage(const std::string& language="auto") override;
+    bool getLanguage(std::string& language) override;
+    bool setVoice(const std::string& voice_name = "auto") override;
+    bool getVoice(std::string& voice_name) override;
+    bool setSpeed(const double speed=0) override;
+    bool getSpeed(double& speed) override;
+    bool setPitch(const double pitch) override;
+    bool getPitch(double& pitch) override;
+    bool synthesize(const std::string& text, yarp::sig::Sound& sound) override;
+
+private:
+    std::shared_ptr<google::cloud::texttospeech_v1::TextToSpeechClient>    m_synthClient{nullptr};
+    std::shared_ptr<google::cloud::texttospeech::v1::SynthesisInput>       m_synthInput{nullptr};
+    std::shared_ptr<google::cloud::texttospeech::v1::VoiceSelectionParams> m_synthVoiceSelParams{nullptr};
+    std::shared_ptr<google::cloud::texttospeech::v1::Voice>                m_synthVoice{nullptr};
+    std::shared_ptr<google::cloud::texttospeech::v1::AudioConfig>          m_synthAudioConfig{nullptr};
+    std::string    m_voiceName;
+    std::string    m_language;
+    double         m_speed;
+    double         m_pitch;
+
 };
 
 #endif // YARP_GOOGLESPEECHSYNTH_H
