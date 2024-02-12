507ebda06dc52a2661d552dddb177ec23cf75236
Ettore Landini <ettore.landini@iit.it>
2024-02-12 15:31:55 +0100

    GoogleDialogFlowCxChatBot device uses YARP device parameters parser
    Also some useles debug print have been removed

--- src/devices/googleDialogflowCxChatBot/GoogleDialogFlowCxChatBot.h
+++ src/devices/googleDialogflowCxChatBot/GoogleDialogFlowCxChatBot.h
@@ -18,6 +18,8 @@
 #include <google/cloud/dialogflow/cx/v3/session.pb.h>
 #include <grpcpp/grpcpp.h>
 
+#include "GoogleDialogFlowCxChatBot_ParamsParser.h"
+
 /**
  *  @ingroup dev_impl_other
  *
@@ -25,51 +27,13 @@
  *
  * \brief `googleDialogFlowCxChatBot`: A yarp device to enable interaction with Google's DialogFlow CX service
  *
- *  Parameters required by this device are:
- * | Parameter name | SubParameter | Type    | Units          | Default Value | Required | Description                                                      | Notes |
- * |:--------------:|:------------:|:-------:|:--------------:|:-------------:|:--------:|:----------------------------------------------------------------:|:-----:|
- * | project        | -            | string  | -              | -             | Yes      | The name of the Google cloud project the wanted agent belongs to |       |
- * | location       | -            | string  | -              | -             | Yes      | The world region specified for the wanted agent                  |       |
- * | agent_name     | -            | string  | -              | -             | Yes      | The name of the agent                                            |       |
- * | language_code  | -            | string  | -              | en-US         | No       | The language code to use for the interaction with the bot        |       |
- *
- *
- * example of xml file with a fake odometer
- *
- * \code{.unparsed}
- * <?xml version="1.0" encoding="UTF-8"?>
- * <!DOCTYPE robot PUBLIC "-//YARP//DTD yarprobotinterface 3.0//EN" "http://www.yarp.it/DTD/yarprobotinterfaceV3.0.dtd">
- * <robot name="googleTest" build="2" portprefix="/googleBot" xmlns:xi="http://www.w3.org/2001/XInclude">
- *     <devices>
- *         <device name="googleDialogCx" type="googleDialogFlowCxChatBot">
- *             <param name="project">
- *                 <!-- Insert project name here -->
- *             </param>
- *             <param name="location">
- *                 <!-- Insert location id here -->
- *             </param>
- *             <param name="agent_name">
- *                 <!-- Insert agent display name here -->
- *             </param>
- *         </device>
- *         <device name="dialogWrap" type="chatBot_nws_yarp">
- *             <action phase="startup" level="5" type="attach">
- *                 <paramlist name="networks">
- *                     <elem name="subdeviceGoogle">
- *                         googleDialogCx
- *                     </elem>
- *                 </paramlist>
- *             </action>
- *             <action phase="shutdown" level="5" type="detach" />
- *         </device>
- *     </devices>
- * </robot>
- * \endcode
+ *  Parameters required by this device are described in class GoogleDialogFlowCxChatBot_ParamsParser
  */
 
 
 class GoogleDialogFlowCxChatBot :
         public yarp::dev::DeviceDriver,
+        public GoogleDialogFlowCxChatBot_ParamsParser,
         public yarp::dev::IChatBot
 {
 public:
@@ -92,11 +56,8 @@ public:
     bool resetBot() override;
 
 private:
-    std::string m_project;
-    std::string m_location;
     std::string m_agentId;
     std::string m_sessionId;
-    std::string m_languageCode;
     std::string m_currentPage;
     std::shared_ptr<grpc::Channel> m_channel;
     std::unique_ptr<google::cloud::dialogflow::cx::v3::Sessions::Stub> m_sessionStub;
---
+++ src/devices/googleDialogflowCxChatBot/GoogleDialogFlowCxChatBot_params.md
@@ -0,0 +1,6 @@
+ | Group name | Parameter name | Type   | Units | Default Value  | Required | Description                                                                                      | Notes                                  |
+ |:----------:|:--------------:|:------:|:-----:|:--------------:|:--------:|:------------------------------------------------------------------------------------------------:|:--------------------------------------:|
+ |            | project        | string  | -     | -            | Yes      | The name of the Google cloud project the wanted agent belongs to |       |
+ |            | location       | string  | -     | -            | Yes      | The world region specified for the wanted agent                  |       |
+ |            | agent_name     | string  | -     | -            | Yes      | The name of the agent                                            |       |
+ |            | language_code  | string  | -     | en-US        | No       | The language code to use for the interaction with the bot        |       |
