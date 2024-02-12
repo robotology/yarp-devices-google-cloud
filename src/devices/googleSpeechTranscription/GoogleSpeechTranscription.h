ad6e0fd5cec6a570b315c06c6028ed98fd5ddc24
Ettore Landini <ettore.landini@iit.it>
2024-02-12 15:37:22 +0100

    GoogleSpeechTranscription device uses YARP device parameters parser

--- src/devices/googleSpeechTranscription/GoogleSpeechTranscription.h
+++ src/devices/googleSpeechTranscription/GoogleSpeechTranscription.h
@@ -17,6 +17,8 @@
 #include "google/cloud/speech/v1/speech_client.h"
 #include "google/protobuf/repeated_ptr_field.h"
 
+#include "GoogleSpeechTranscription_ParamsParser.h"
+
 
 /**
  *  @ingroup dev_impl_other
@@ -25,42 +27,13 @@
  *
  * \brief `googleSpeechTranscription`: A yarp device for speech transcription using google cloud cpp libraries
  *
- *  Parameters required by this device are:
- * | Parameter name | SubParameter | Type    | Units          | Default Value | Required | Description                                        | Notes |
- * |:--------------:|:------------:|:-------:|:--------------:|:-------------:|:--------:|:--------------------------------------------------:|:-----:|
- * | language_code  | -            | string  | -              | -             | Yes      | Language for speech synthesis (e.g. "ita", "eng")  |       |
+ *  Parameters required by this device are described in class GoogleSpeechTranscription_ParamsParser
  *
- *
- * example of xml file with a fake odometer
- *
- * \code{.unparsed}
- * <?xml version="1.0" encoding="UTF-8"?>
- * <!DOCTYPE robot PUBLIC "-//YARP//DTD yarprobotinterface 3.0//EN" "http://www.yarp.it/DTD/yarprobotinterfaceV3.0.dtd">
- * <robot name="googleTest" build="2" portprefix="/googleTranscr" xmlns:xi="http://www.w3.org/2001/XInclude">
- *     <devices>
- *         <device name="googleTranscr" type="googleSpeechTranscription">
- *             <param name="language_code">
- *                 it-IT
- *             </param>
- *         </device>
- *
- *         <device name="synthWrap" type="speechTranscription_nws_yarp">
- *             <action phase="startup" level="5" type="attach">
- *                 <paramlist name="networks">
- *                     <elem name="subdeviceGoogle">
- *                         googleTranscr
- *                     </elem>
- *                 </paramlist>
- *             </action>
- *             <action phase="shutdown" level="5" type="detach" />
- *         </device>
- *     </devices>
- * </robot>
- * \endcode
  */
 
 class GoogleSpeechTranscription :
         public yarp::dev::DeviceDriver,
+        public GoogleSpeechTranscription_ParamsParser,
         public yarp::dev::ISpeechTranscription
 {
 public:
@@ -81,9 +54,7 @@ public:
     bool transcribe(const yarp::sig::Sound& sound, std::string& transcription, double& score) override;
 
 private:
-    int          m_sampleRate;
     bool         m_offline{false};
-    std::string  m_languageCode;
     google::cloud::speech::v1::RecognitionConfig             m_audioConfig;
     std::shared_ptr<google::cloud::speech_v1::SpeechClient>  m_client{nullptr};
 };
